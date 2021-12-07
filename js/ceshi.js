5db5f724f20131e50f51c1828ded8a3199c30afe
yangyifan23 <81276269+yangyifan23@users.noreply.github.com>
2021-12-07 20:30:36 +0800

    Add files via upload

---
+++ js/ceshi.js
@@ -0,0 +1,18 @@
+// var yexiaofan = {
+//     name:"叶小凡",
+//     age:16,
+//     eat:function () {
+//         console.log("KFC");
+//     }
+// }
+// for(var p in yexiaofan){
+//     console.log(p+"="+yexiaofan)
+// }
+
+for(let a of 'hello'){
+    console.log(a)
+}
+
+for(var i=0;i<10;i++){
+    console.log(i)
+}
